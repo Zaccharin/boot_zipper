1fa24742e82a8c556919f3868d02debcdb08fa2e
cancel {"'} <166788148+Zaccharin@users.noreply.github.com>
2024-08-28 20:03:10 -0700

    Update and rename launch.json to content.jsx
    ./udp, ./gov, ./boot, .zipper.exe

--- content.jsx
+++ content.jsx
@@ -1,11 +1,11 @@
 {
-    "/*
-    {"Project Name":"CARBON.TERMINAL.PROJECT"}: {
+    /*
+    "{"Project Name":"CARBON.TERMINAL.PROJECT"}: {
       {"Description":"./udp, /gov, ./boot.ziipper.exe"}
       {"Host IP":"192.16.184.0"}-:-{"Port":"22"}
       {"Username":""}|{"Private Key":""} // {"Password":""}|{"Private Key Password"}
         {"@HTTP://http"}-:-{"(resolve)"}-:-{"@HTTPS://https"};
       }
-    },
-    */"
+    },"
+    */
 }
